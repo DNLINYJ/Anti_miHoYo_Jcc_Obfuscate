714994aa44db4454b19b96368bfc126dd4ec3a6e
DNLINYJ <linqian9687654321@gmail.com>
2021-07-07 23:41:22 +0800

    删除无用函数

--- YS x64dbg Plugin/GetWebIndex.cpp
+++ YS x64dbg Plugin/GetWebIndex.cpp
@@ -43,37 +43,3 @@ string get_web(string web_url)
         return strTmpStr;
     }
 }
-
-string post_web(string web_url, string data)
-{
-    CURL* curl; //定义CURL类型的指针
-    CURLcode res;  //定义CURLcode类型的变量，保存返回状态码
-
-    curl = curl_easy_init();  //初始化一个CURL类型的指针,一般curl_easy_init意味着一个会话的开始
-    if (curl != NULL)
-    {
-        struct curl_slist* headers = NULL;
-
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1); //解决301、302跳转问题
-        //设置curl选项. 其中CURLOPT_URL是让用户指定url. argv[1]中存放的命令行传进来的网址
-        curl_easy_setopt(curl, CURLOPT_URL, web_url.c_str());
-        curl_easy_setopt(curl, CURLOPT_POST, 1); //设置为非0表示本次操作为POST
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str()); // POST数据传入
-
-        string strTmpStr;
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, downloadCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &strTmpStr);
-
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);//忽略证书检查
-
-        res = curl_easy_perform(curl);
-
-        //cout << strTmpStr.c_str();
-
-        if (res != CURLE_OK) //如果有错误，打印出错误信息
-            cout << curl_easy_strerror(res);
-
-        curl_easy_cleanup(curl); //这个调用用来结束一个会话.与curl_easy_init配合着用.
-        return strTmpStr;
-    }
-}
--- YS x64dbg Plugin/GetWebIndex.h
+++ YS x64dbg Plugin/GetWebIndex.h
@@ -2,4 +2,3 @@
 #include <string>
 
 std::string get_web(std::string web_url);
-std::string post_web(std::string web_url, std::string data);
